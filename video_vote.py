4db054328f2d0cccdfe14e5daf3b645e7b77afa5
hoetaekpro <hoetaek@student.snue.ac.kr>
2019-12-14 15:41:02 +0900

    vote under 3min delay

--- video_vote.py
+++ video_vote.py
@@ -6,7 +6,7 @@ import random
 def close_popup(driver):
     parent_h = driver.current_window_handle
     # click on the link that opens a new window
-    handles = driver.window_handles # before the pop-up window closes
+    handles = driver.window_handles  # before the pop-up window closes
     handles.remove(parent_h)
     driver.switch_to.window(handles.pop())
     driver.find_element_by_css_selector('div.tmp-footer > button').click()
@@ -18,7 +18,7 @@ def close_popup(driver):
 
 def vote(usr, pw, url):
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+    # options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
     options.add_argument("--disable-notifications")
@@ -46,11 +46,14 @@ def vote(usr, pw, url):
                     driver = close_popup(driver)
     driver.get(url)
     time.sleep(4)
-    driver.find_element_by_css_selector('#page-wrapper > div > div.event03 > div > div.list_cont > div > button > p').click()
+    driver.find_element_by_css_selector(
+        '#page-wrapper > div > div.event03 > div > div.list_cont > div > button > p').click()
+    time.sleep(2)
     driver.quit()
 
 
 def start_voting(users):
+    users.reverse()
     for i, user in enumerate(users, start=1):
         usr, pw = user
         print(i, usr, pw)
@@ -61,7 +64,7 @@ def start_voting(users):
         vote(usr, pw, 'https://ssam.teacherville.co.kr/?mode=event.schoolstarts&sub=teacher&keyword=교사에대하여')
         time.sleep(random.randint(2, 10))
         if i % 3 == 0:
-            t = random.randint(300, 600)
+            t = random.randint(100, 200)
             print(time.localtime())
             print(f"{t//60}분 동안 쉬어갑니다~")
             time.sleep(t)
